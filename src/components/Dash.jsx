76a37a442c319872a4d497e843d63aa147ba22b1
moustafa abdelati <mustafaabdelati@gmail.com>
2026-01-02 18:24:56 +1100

    commit

--- src/components/Dash.jsx
+++ src/components/Dash.jsx
@@ -1,10 +1,15 @@
 export default function Dash() {
   return (
     <>
-      <div className=" wrapper grid grid-col:5">
-        <div className="left bg-green-900">xxxx</div>
+      <div className=" wrapper bg-orange-900 ">
+        <div className="grid grid-cols-4 gap-8">
+          <div className="left bg-green-900 grid">
+            <div>LEFT-top</div>
+            <div>LEFT-bottom</div>
+          </div>
 
-        <div className="right bg-blue-900 ">xxxx</div>
+          <div className="right bg-blue-900 ">RIGHT</div>
+        </div>
       </div>
     </>
   );
