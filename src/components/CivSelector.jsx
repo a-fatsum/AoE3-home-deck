76bfd8c0dd92be203687d868a0a51e54a071a6d9
moustafa abdelati <mustafaabdelati@gmail.com>
2026-01-05 22:53:56 +1100

    created basic radio selection to select age

--- src/components/CivSelector.jsx
+++ src/components/CivSelector.jsx
@@ -21,6 +21,23 @@ export default function CivSelector({
           ))}
         </select>
       </div>
+
+      {/* Age selections */}
+
+      <div>
+        <input type="radio" id="age1" name="age" value="Age1" />
+        <label htmlFor="age1"> I: Exploration Age</label>
+        <br />
+        <input type="radio" id="age2" name="age" value="Age2" />
+        <label htmlFor="age2"> II: Commerce Age</label>
+        <br />
+        <input type="radio" id="age3" name="age" value="Age3" />
+        <label htmlFor="age3">III: Fortress Age</label>
+        <br />
+        <input type="radio" id="age4" name="age" value="Age4" />
+        <label htmlFor="age4"> V: Industerial Age</label>
+        <br />
+      </div>
     </div>
   );
 }
